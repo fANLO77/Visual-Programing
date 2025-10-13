66c2af93e828c67f1862c8c796b98452670c61f7
fANLO77 <20060803d@gmail.com>
2025-10-13 10:41:49 +0700

    ㅤ

--- src/Human.kt
+++ src/Human.kt
@@ -2,24 +2,25 @@ import kotlin.math.*
 import kotlin.random.*
 
 open class Human(
-    private val fullName: String, // ФИО человека
-    private val age: Int, // возраст
-    override val currentSpeed: Double // текущая скорость
+    private val fullName: String, 
+    private val age: Int, 
+    override val currentSpeed: Double 
 ) : Movable {
-    // начальные координаты
+    
     override var x: Double = 0.0
     override var y: Double = 0.0
 
-    // Функция рандомного движения
+    
     override fun move() {
         val direction = Random.nextDouble(0.0, 2 * PI)
-        // обновление координват
+        
         x += currentSpeed * cos(direction)
         y += currentSpeed * sin(direction)
     }
 
-    // получение значений
-    override fun getFullName(): String = fullName // Переопределяем с override
+    
+    override fun getFullName(): String = fullName 
     override fun getCurrentSpeed(): Double = currentSpeed
     override fun getPosition(): Pair<Double, Double> = Pair(x, y)
+
 }
